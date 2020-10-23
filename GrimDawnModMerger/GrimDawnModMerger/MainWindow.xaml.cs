49fa46ec0eb89befbfc18c7951afc798fc391ac7
Ian Chapman <gammaian@yahoo.com>
2020-10-22 21:56:56 -0700

    Remove unused usings

--- GrimDawnModMerger/GrimDawnModMerger/MainWindow.xaml.cs
+++ GrimDawnModMerger/GrimDawnModMerger/MainWindow.xaml.cs
@@ -5,17 +5,8 @@ using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace GrimDawnModMerger
 {
@@ -95,7 +86,6 @@ namespace GrimDawnModMerger
                     activeMods.Add(item.Name);
                     Trace.WriteLine(item.Name + ", is checked: " + item.IsSelected);
                 }
-
             }
             btnMerge.Content = "MERGE IN\nPROGRESS";
             btnMerge.IsEnabled = false;
@@ -112,7 +102,7 @@ namespace GrimDawnModMerger
             modList.Clear();
             foreach(DirectoryInfo d in dirInfo.GetDirectories())
             {
-                if (d.Name.Equals("gdx1") || d.Name.Equals("gdx2") || d.Name.Equals("ModManagerCombined"))// TODO change the last one to not be magic
+                if (d.Name.Equals("gdx1") || d.Name.Equals("gdx2"))
                     continue;
                 modList.Add(new ModListItem(false, d.Name));
             }
